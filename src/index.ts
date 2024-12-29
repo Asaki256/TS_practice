ec1b7155e95384111f48143d2ee38597842971f2
Masaki <ryota423.jp@icloud.com>
2024-12-29 21:40:55 +0900

    2.1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
+// npx tsc
+  // トランスパイル
+// npx tsc -w
+  // 保存のたびにコンパイル。--watchでも可。
+// node dist
+  // JS実行
+
 const message: string = "Hello world!";
 // message = 12;
 const test: number = 123
-console.log(test);
+// console.log(test);
 
+// console.log(message);
+// console.log(message);
 
-console.log(message);
+const result = console.log('test');
+console.log(result);
+
+console.log(typeof console.log);
+console.log(Object.getOwnPropertyDescriptor(console, 'log'));
+
+const boundLog = console.log.bind(null);
+console.log(boundLog);
+
+console.log(console.log('test'));
